e6db5b339f09cf635ea8fdb9397acba752117174
anon <anon@anon.co.uk>
2017-03-24 22:51:38 +0000

    Files now read in

---
+++ ParallelComputing/FileIO.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "FileIO.h"
+
+
+FileIO::FileIO()
+{
+}
+
+
+FileIO::~FileIO()
+{
+}
--- ParallelComputing/ParallelComputing.cpp
+++ ParallelComputing/ParallelComputing.cpp
@@ -4,10 +4,13 @@
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #define __CL_ENABLE_EXCEPTIONS
 
+#define DATASET_LENGTH 18732 
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include "Utils.h"
+#include "DataSet.h"
 
 #ifdef __APPLE__
 #include <OpenCL/cl.hpp>
@@ -15,16 +18,59 @@
 #include <CL/cl.hpp>
 #endif
 
-void readInData();
-void printMenu(bool);
-int getMenuChoice(bool);
+void runOperations(int);
+void algorithmOne();
+void readDataFromFile(std::string);
+double minimum(cl::Context, cl::CommandQueue, cl::Program);
+double maximum(cl::Context, cl::CommandQueue, cl::Program);
+double average(cl::Context, cl::CommandQueue, cl::Program);
+double standard_deviation(cl::Context, cl::CommandQueue, cl::Program, double);
+
+std::vector<std::string> locations;
+std::vector<int> years;
+std::vector<int> months;
+std::vector<int> days;
+std::vector<double> temps;
 
 int main()
+{
+	char userChoice;
+	do
+	{
+		std::cout << "--------------------------------------------------" << std::endl;
+		std::cout << "--> Sam D Kershaw - KER14468703" << std::endl;
+		std::cout << "--> 1) Algorithm 1" << std::endl;
+		std::cout << "--> 2) Algorithm 2" << std::endl;
+		std::cout << "--> 0) Quit the Program" << std::endl;
+		std::cin >> userChoice;
+		std::cout << "--------------------------------------------------" << std::endl;
+		switch (userChoice)
+		{
+		case '1':
+			std::cout << "Running Algorithm One..." << std::endl;
+			algorithmOne();
+			break;
+		default:
+			break;
+		}
+	} while (userChoice != '0');
+
+	return 0;
+}
+
+void runOperations(int algorithm=1) {
+	
+}
+
+void algorithmOne()
 {
 	int platform_id = 0;
 	int device_id = 0;
 
-	int userChoice = getMenuChoice(false);
+	readDataFromFile("temp_lincolnshire_short.txt");
+
+	char c;
+	std::cin >> c;
 
 	cl::Context context = GetContext(platform_id, device_id);
 
@@ -47,39 +93,89 @@ int main()
 		std::cout << "Build Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
 		throw err;
 	}
-
-
-    return 0;
 }
 
-void readInData()
+void readDataFromFile(std::string filePath)
 {
-}
+	std::ifstream txtFile(filePath);
+	
+	int lineCount = 0;
 
-int getMenuChoice(bool clear)
-{
-	int userChoice = -1;
-	while (userChoice < 1 || userChoice > 3)
+	if (txtFile.is_open()) 
 	{
-		printMenu(false);
-		if (!(cin >> userChoice))
+		std::string item, line;
+
+		std::cout << "Lines Read: " << lineCount << "...";
+
+		while (getline(txtFile, line))
 		{
-			cin.clear(); //clear the failure flag if there is an error when reading!
-			string garbage;
-			std::getline(cin, garbage); //read the garbage from the stream and throw it away
-			return -1;
+			std::string location;
+			stringstream _line = stringstream(line);
+
+			int count = 0;
+			while (getline(_line, item, ' '))
+			{
+				switch (count)
+				{
+				case 0:
+					//location
+					locations.push_back(item);
+					count++;
+					break;
+				case 1:
+					//year
+					years.push_back(stoi(item));
+					count++;
+					break;
+				case 2:
+					//month
+					months.push_back(stoi(item));
+					count++;
+					break;
+				case 3:
+					days.push_back(stoi(item));
+					count++;
+				case 4:
+					temps.push_back(stod(item));
+					count = 0;
+					break;
+				default:
+					count = 0;
+					break;
+				}
+			}
+			lineCount++;
+			if (lineCount % 100000 == 0) {
+				std::cout << "\rLines Read: " << lineCount << "...";
+			}
+			//std::cout << "\rLines Read: " << lineCount;
 		}
 	}
-	return -1;
+	else {
+		std::cout << "Couldn't open the file!!";
+	}
+
+	txtFile.close();
+
+	std::cout << "\rRead " << lineCount << " lines in successfully." << std::endl;
 }
 
-void printMenu(bool clear)
+double minimum(cl::Context ctxt, cl::CommandQueue q, cl::Program prg)
 {
-	if (clear) {
-		system("cls");
-	}
-	std::cout << "Parallel Computing ~ Assignment/r/r" << std::endl;
-	std::cout << "1 - Menu Item 1" << std::endl;
-	std::cout << std::endl;
-	std::cout << "> ";
+	return 0.0;
+}
+
+double maximum(cl::Context ctxt, cl::CommandQueue q, cl::Program prg)
+{
+	return 0.0;
+}
+
+double average(cl::Context ctxt, cl::CommandQueue q, cl::Program prg)
+{
+	return 0.0;
+}
+
+double standard_deviation(cl::Context ctxt, cl::CommandQueue q, cl::Program prg, double avg)
+{
+	return 0.0;
 }
